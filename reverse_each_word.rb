0fce995d2f8b19bd8afa3fd87955496cb51c9e5f
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-25 08:27:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,20 @@
+# require "pry"
+
 def reverse_each_word(array)
-  new_arr = array.split(" ")
+  new_arr = array.split
   new_arr.collect do |x| 
     x.reverse!
     end
   new_arr.join(" ")
 end
+
+#### USING .EACH ####
+
+# def reverse_each_word(array)
+#   empty = []
+#   new_arr = array.split
+#   new_arr.each do |x| 
+#     empty.push(x.reverse!)
+#     end
+#   empty.join(" ")
+# end
